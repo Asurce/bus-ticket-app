02f93a8f1b790ea766b47331f2c4d70419f28308
Asurce <bogen.bence@gmail.com>
2024-05-10 21:53:35 +0200

    update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,19 +7,13 @@ import {getAuth, provideAuth} from '@angular/fire/auth';
 import {getFirestore, provideFirestore} from '@angular/fire/firestore';
 import {getStorage, provideStorage} from '@angular/fire/storage';
 import {AngularFireModule} from "@angular/fire/compat";
+import {environment} from "../environments/environment";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
-    importProvidersFrom(AngularFireModule.initializeApp({
-      "projectId": "angularbusticketapp",
-      "appId": "1:783742674408:web:118b221a208da7913db05d",
-      "storageBucket": "angularbusticketapp.appspot.com",
-      "apiKey": "AIzaSyAPC9rIFj6tPskz4gp2D8H8F6pmZZ4rPpk",
-      "authDomain": "angularbusticketapp.firebaseapp.com",
-      "messagingSenderId": "783742674408"
-    })),
+    importProvidersFrom(AngularFireModule.initializeApp(environment.firebase)),
     importProvidersFrom(provideAuth(() => getAuth())),
     importProvidersFrom(provideFirestore(() => getFirestore())),
     importProvidersFrom(provideStorage(() => getStorage()))]
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    "projectId": "angularbusticketapp",
+    "appId": "1:783742674408:web:118b221a208da7913db05d",
+    "storageBucket": "angularbusticketapp.appspot.com",
+    "apiKey": "AIzaSyAPC9rIFj6tPskz4gp2D8H8F6pmZZ4rPpk",
+    "authDomain": "angularbusticketapp.firebaseapp.com",
+    "messagingSenderId": "783742674408"
+  }
+}
